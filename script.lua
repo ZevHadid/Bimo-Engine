04c413adfd4d5359164b87427e057b5f0f81130d
Zev Hadid <zevhadid@gmail.com>
2025-08-26 12:43:35 +0700

    Updated lau example code

--- script.lua
+++ script.lua
@@ -1,10 +1,3 @@
-
-function sleep(seconds)
-    local start_time = os.time()
-    while os.time() < start_time + seconds do
-        -- Loop until the desired time has passed
-    end
-end
 -- Rectangle in the center
 rect = MakeShape('<rect x="300" y="250" width="200" height="100" fill="red"/>')
 -- Circle in the center
@@ -27,4 +20,5 @@ line = MakeShape('<line x1="50" y1="500" x2="750" y2="500" stroke="red"/>')
 line:show()
 
 print("All shapes created and shown!")
+
 
